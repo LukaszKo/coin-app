4f48048fb094f0c574ae1dc8ebc05ccf2a6d123e
Lukasz_Kochajewski <lukasz.kochajewski@gmail.com>
2020-06-30 15:07:25 +0200

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <slot />
+    <div class="searchbox">
+      <SearchInput placeholder="Search" />
+      <img src="../assets/search.png" />
+    </div>
+  </div>
+</template>
+<script>
+import SearchInput from "@/components/SearchInput";
+export default {
+  components: { SearchInput }
+};
+</script>
+<style lang="scss" scoped>
+@import "@/scss/_tokens.scss";
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.searchbox {
+  display: flex;
+
+  img {
+    margin-left: $spacing-small;
+  }
+}
+</style>
