94e5caccb5c2eaa67ca67719e810095368fb4d64
Lukasz_Kochajewski <lukasz.kochajewski@gmail.com>
2020-06-30 15:08:12 +0200

    create homepage update default layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Navbar />
+    <router-view class="container" />
+    <Footer />
   </div>
 </template>
-
+<script>
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+export default {
+  components: { Navbar, Footer }
+};
+</script>
 <style lang="scss">
+@import "@/scss/_tokens.scss";
+
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: #f4f4f4;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  height: 100%;
+  padding: $spacing-large 10% 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,137 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Header>
+      <div class="title">
+        <h3>Etam eniam dolor epsilon</h3>
+        <span>{{ count }} crypto coins</span>
+      </div>
+    </Header>
+
+    <div class="list">
+      <div class="table__headers">
+        <div
+          class="table__headers__item"
+          v-for="header in tableHeaders"
+          :key="header"
+        >
+          {{ header }}
+        </div>
+      </div>
+      <div class="table__content">
+        <ListItem
+          v-for="(item, index) in paginatedList"
+          :key="item.id + index"
+          :item="item"
+          @click="goToEditPage(item.id)"
+        />
+      </div>
+    </div>
+    <div class="next-page">
+      <img @click="next" src="../assets/nextPage.png" alt="next page" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ListItem from "@/components/ListItem";
+import Header from "@/components/Header";
 
+import axios from "axios";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: { ListItem, Header },
+  data() {
+    return {
+      tableHeaders: [
+        "Nazwa",
+        "Cena USD",
+        "Cena BTC",
+        "Cena ETH",
+        "Market Cap(24h)(USD)"
+      ],
+      list: [],
+      all: [],
+      page: 0,
+      limit: 10
+    };
+  },
+  computed: {
+    count() {
+      return this.all.length;
+    },
+    limited() {
+      return this.page ? (this.page + 1) * 10 : 10;
+    },
+    paginatedList() {
+      return this.all.filter(
+        (item, index) => index >= this.page * 10 && index < this.limited
+      );
+    }
+  },
+  async created() {
+    const page = this.$router.currentRoute.query.page;
+    if (page) {
+      this.page = page - 1;
+    }
+
+    const { data } = await axios.get("tickers?quotes=USD,BTC,ETH");
+    this.all = data;
+    this.updateQuery();
+  },
+  methods: {
+    goToEditPage(id) {
+      this.$router.push({ name: "Details", params: { id } });
+    },
+    next() {
+      this.page++;
+      this.updateQuery();
+    },
+    updateQuery() {
+      this.$router.replace({ query: { page: this.page + 1 } }).catch(() => {});
+    },
+    updateList() {
+      this.list = this.all.slice(this.limit * this.page, this.limit);
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+@import "@/scss/_tokens.scss";
+
+.title {
+  text-align: left;
+}
+
+.panel {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.list {
+  margin: $spacing-large 0;
+  min-height: 70vh;
+}
+
+.table {
+  &__headers {
+    display: flex;
+    justify-content: space-between;
+
+    &__item {
+      flex: 1;
+      padding: $spacing-small;
+      font-weight: 600;
+    }
+  }
+
+  &__content {
+    margin: $spacing-small 0 $spacing-large;
+  }
+}
+.next-page {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
