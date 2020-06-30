96d40ca4e5e4cece0e0b76fbf4533b4ce6e31fbb
Lukasz_Kochajewski <lukasz.kochajewski@gmail.com>
2020-06-30 15:08:20 +0200

    create search input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <input :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "@/scss/_tokens.scss";
+
+input {
+  outline: none;
+  border: none;
+  padding: $spacing-small $spacing-medium;
+  color: #bfbfc0;
+}
+</style>
