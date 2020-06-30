361d7d72dc4dbd70ec58738ca083904984e50f35
Lukasz_Kochajewski <lukasz.kochajewski@gmail.com>
2020-06-30 15:06:15 +0200

    add custom filters create scss tokens add global css utils helpers

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+
+const BASE_URL = process.env.VUE_APP_BASE_URL;
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = BASE_URL;
 
+Vue.filter("uppercase", val => {
+  if (!val) return "";
+  return val.toUpperCase();
+});
+
+Vue.filter("currency", val => {
+  if (!val) return "";
+  return `$ ${val}`;
+});
+
+Vue.filter("fixed", (val, param) => {
+  if (!val || !param) return "";
+  return val.toFixed(param);
+});
+
+import "@/scss/custom.scss";
 new Vue({
   router,
   render: h => h(App)
